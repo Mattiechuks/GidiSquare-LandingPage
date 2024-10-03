0c679b382d3d3b85aaa571714bc9db427daff994
Chukwuji Matthew Olisenekwu <73289923+Mattiechuks@users.noreply.github.com>
2024-10-03 11:17:51 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import NavBar from "@/components/NavBar";
 import HeroSection from "@/components/HeroSection";
 import AboutUs from "@/components/AboutUs";
 import AppPromotion from "@/components/AppPromotion";
-import Waitlist from "@/components/waitlist";
+import Waitlist from "@/components/Waitlist";
 import Footers from "@/components/Footers";
 
 
